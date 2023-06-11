d0dd824ec887d92ad06d445c738a3cec815adb4c
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-10 22:35:30 -0300

    Calculadora em C

---
+++ Calculadora.c
@@ -0,0 +1,41 @@
+//Em progresso
+
+#include <stdio.h>
+
+int main(void){
+    float num1,num2,resultado;
+    int oper,rep;
+    rep = 1;
+    while(rep==1){
+        printf("Menu de calculos");
+        printf("\n1-Soma");
+        printf("\n2-Subtração");
+        printf("\n3-Divisão");
+        printf("\n4-Multiplicação");
+        printf("\nEscolha uma opção: ");
+        scanf("%d", oper);
+        printf("Digite um numero: ");
+        scanf("%f", num1);
+        printf("Digite outro numero: ");
+        scanf("%f", num2);
+        if(oper==1){
+            resultado = num1+num2;
+            printf("O resultado é %f", resultado);
+        }
+            if(oper==2){
+            resultado = num1-num2;
+            printf("O resultado é %f", resultado);
+        }
+            if(oper==3){
+            resultado = num1/num2;
+            printf("O resultado é %f", resultado);
+        }
+            if(oper==4){
+            resultado = num1*num2;
+            printf("O resultado é %f", resultado);
+        }
+        printf("Deseja fazer mais um calculo(Sim = 1/Não = 0)?: ");
+        scanf("%d", rep);
+    }
+    printf("Programa finalizado");
+}
