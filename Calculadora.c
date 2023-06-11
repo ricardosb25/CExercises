450c2d73734bb1de4ed53077270c34a9cc7aee47
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-10 22:56:57 -0300

    Update Calculadora.c
    Funcional

--- Calculadora.c
+++ Calculadora.c
@@ -7,35 +7,36 @@ int main(void){
     int oper,rep;
     rep = 1;
     while(rep==1){
-        printf("Menu de calculos");
+        printf("\n\nMenu de calculos");
         printf("\n1-Soma");
         printf("\n2-Subtração");
         printf("\n3-Divisão");
         printf("\n4-Multiplicação");
         printf("\nEscolha uma opção: ");
-        scanf("%d", oper);
+        scanf("%d", &oper);
         printf("Digite um numero: ");
-        scanf("%f", num1);
+        scanf("%f", &num1);
         printf("Digite outro numero: ");
-        scanf("%f", num2);
+        scanf("%f", &num2);
         if(oper==1){
             resultado = num1+num2;
-            printf("O resultado é %f", resultado);
+            printf("O resultado é %.2f", resultado);
         }
             if(oper==2){
             resultado = num1-num2;
-            printf("O resultado é %f", resultado);
+            printf("O resultado é %.2f", resultado);
         }
             if(oper==3){
             resultado = num1/num2;
-            printf("O resultado é %f", resultado);
+            printf("O resultado é %2.f", resultado);
         }
             if(oper==4){
             resultado = num1*num2;
-            printf("O resultado é %f", resultado);
+            printf("O resultado é %.2f", resultado);
         }
-        printf("Deseja fazer mais um calculo(Sim = 1/Não = 0)?: ");
-        scanf("%d", rep);
+        printf("\nDeseja fazer mais um calculo(Sim = 1/Não = 0)?: ");
+        scanf("%d", &rep);
     }
-    printf("Programa finalizado");
+    printf("\n\nPrograma finalizado");
+    return 0;
 }
