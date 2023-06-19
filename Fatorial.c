38ffdf795c4a7e3e5cbe03330cf280db9f9bf8e7
RicardoSB <130787942+ricardosb25@users.noreply.github.com>
2023-06-18 23:19:24 -0300

    Update Fatorial.c

--- Fatorial.c
+++ Fatorial.c
@@ -1,29 +1,5 @@
 #include <stdio.h>
 
-int fatorial(float x,float y){
-    if(x==0){
-        return 1;
-    }
-    else{
-        while(x!=0){
-            y=x * x-1;
-        }
-        return y;
-    }
-}
-
-int main(void){
-    float x,y,resultado;
-    printf("Digite o seu numero: ");
-    scanf("%f",&x);
-    resultado = fatorial(x,y);
-    printf("O fatorial de %.2f",x, " é: %.2f",resultado);
-    return 0;
-}
-
-
-/*#include <stdio.h>
-
 int fatorial(int x){
     if(x==0){
         return 1;
@@ -45,4 +21,4 @@ int main(void){
     resultado = fatorial(x);
     printf("O fatorial de %d é: %d",x,resultado);
     return 0;
-}*/
+}
